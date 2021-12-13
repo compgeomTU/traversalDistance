e897ec70108a72abffef82f933611a2c14cf10ad
Erfan Hosseini <erfanh1995@aol.com>
2021-12-13 14:50:05 -0600

    add test

--- ReadGraph.py
+++ ReadGraph.py
@@ -1,14 +1,20 @@
 import geojson
 from geojson import LineString, Feature, FeatureCollection
 from Graph import Graph
+from CalFreeSpace import calfreespace
+import FreeSpaceGraph
 
 g = Graph("G")
-#h = Graph("H")
+h = Graph("H")
 
+fsg = FreeSpaceGraph.FreeSpaceGraph(g,h)
+cb = fsg.cell_boundaries(0,0,False)
+fsg.DFSTraversalDist(cb)
 #prints graph objects with no print override 
 #print(g)
 #print(h)
 
 #gives 2 empty files
 #g.Dump2txt("out_g")
+
 
