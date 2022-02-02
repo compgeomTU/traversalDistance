369418b4f6f2cae7e3160024f69b580d29107225
Rena Repenning <renareps108@gmail.com>
2022-02-01 18:54:45 -0600

    fix graph passing in cell bound class

--- FreeSpaceGraph.py
+++ FreeSpaceGraph.py
@@ -3,38 +3,30 @@ import pdb
 
 
 class FreeSpaceGraph:
-    def __init__(self, g1, g2, epsilon):  # g1, g2 are Graph objects
-        self.g1 = g1
+    def __init__(self, g1, g2, epsilon):
+        self.g1 = g1  # g1, g2 are Graph objects
         self.g2 = g2
+        self.e = epsilon
         self.cell_boundaries = {}
-        self.epsilon = epsilon
 
         # get this dynamically, for each --> given one fsbound print the adgacent
         # Horizontal boundaries
-        # for e, v in self.g1.edges, self.g2.nodes:
         for v in self.g2.nodes.keys():
             for e in self.g1.edges.keys():
-                '''
-                use ids to send actual vertices and edges rather than just passing ids
-                '''
-                self.cell_boundaries[(v, e, True)] = self.CellBoundary(
-                    v, e, True)
+                '''myKey = v, e, g1, g2'''
+                #### self.cell_boundaries[(myKey)] = self.CellBoundary(myKey)
+                self.cell_boundaries[(v, e, g1, g2)] = CellBoundary(
+                    v, e, g1, g2, self.e)
         # Verticle boundaries
-        # for e, v in self.g2.edges, self.g1.nodes:
         for v in self.g1.nodes.keys():
             for e in self.g2.edges.keys():
-                self.cell_boundaries[(v, e, False)] = self.CellBoundary(
-                    v, e, False)
-        print("-- Cell Boundaries --\n", self.cell_boundaries)
+                self.cell_boundaries[(v, e, g2, g1)] = CellBoundary(
+                    v, e, g2, g1, self.e)
+
+        '''print("-- Cell Boundaries --\n", print(self.cell_boundaries))'''
 
         # get traversal distance using dfs search
         # given one free space boundary, compute all adjacent free space boundaries
-
-        def getG1():
-            return g1
-
-        def getG2():
-            return g2
 
     def DFSTraversalDist(self, cb):
         def DFS(cb):
@@ -107,23 +99,16 @@ class FreeSpaceGraph:
             i.visited = False
         self.DFS(cb)
 
+        print("\ndone DFS traversal dist fxn")
+
 
 class CellBoundary:
-    '''def __init__(self, vertex, edge, isHoriz):'''
 
-    def __init__(self, vertexID, edgeID, isHoriz):
+    def __init__(self, vertexID, edgeID, g_edge, g_verts, eps):
         # use ID's consistant with Erfan's code
         self.vertexID = vertexID
         self.edgeID = edgeID
-        self.isHoriz = isHoriz  # True if the edge is from G1
         self.visited = False
-
-        if isHoriz:  # edge is from G1
-            g_edge = super.g1
-            g_verts = super.g2
-        else:  # edge is from G2
-            g_edge = super.g2
-            g_verts = super.g1
 
         edge = g_edge.edges[self.edgeID]
         # inputs for CFS
@@ -133,10 +118,9 @@ class CellBoundary:
         y2 = g_edge.nodes[edge[1]][0]
         xa = g_verts.nodes[vertexID][0]
         ya = g_verts.nodes[vertexID][1]
-        # call CFS and return tuple
-        # compute from free space by traversing the free space
+        # call CFS and return tuple --> compute from free space by traversing the free space
         self.start, self.end = calfreespace(
-            x1, y1, x2, y2, xa, ya, super.epsilon)
+            x1, y1, x2, y2, xa, ya, eps)
 
     def print_cellboundary(self):
         print("Cell Boundary: ", self.vertexID, self.edgeID, self.isHoriz)
