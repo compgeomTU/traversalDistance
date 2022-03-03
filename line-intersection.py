01945a86fbf531e38753d96f4539a3e30619d473
Erfan Hosseini <erfanh1995@aol.com>
2022-03-02 19:07:45 -0600

    returns corres end point if max/min is out of segm

--- line-intersection.py
+++ line-intersection.py
@@ -40,22 +40,61 @@ def find_ellipse_max_min_points(line1, line2, epsilon):
 
     # line 1
     if m1 == math.inf:
-        max1 = (inter_x,inter_y+d)
-        min1 = (inter_x,inter_y-d)
+        if inter_y+d > max(line1[0][1], line1[1][1]):
+            max1 = (inter_x, max(line1[0][1], line1[1][1]))
+        else:
+            max1 = (inter_x,inter_y+d)
+        if inter_y-d < min(line1[0][1], line1[1][1]):
+            min1 = (inter_x, min(line1[0][1], line1[1][1]))
+        else:
+            min1 = (inter_x,inter_y-d)
     else:
         x1 = d/math.sqrt(m1*m1+1)
         y1 = m1*x1
-        max1 = (inter_x+x1,inter_y+y1)
-        min1 = (inter_x-x1,inter_y-y1)
+        if inter_x+x1 > max(line1[0][0], line1[1][0]):
+            if line1[0][0] > line1[1][0]:
+                max1 = (line1[0][0], line1[0][1])
+            else:
+                max1 = (line1[1][0], line1[1][1])
+        else:
+            max1 = (inter_x+x1,inter_y+y1)
+        
+        if inter_x-x1 < min(line1[0][0], line1[1][0]):
+            if line1[0][0] < line1[1][0]:
+                min1 = (line1[0][0], line1[0][1])
+            else:
+                min1 = (line1[1][0], line1[1][1])
+        else:
+            min1 = (inter_x-x1,inter_y-y1)
+
     # line 2
     if m2 == math.inf:
-        max2 = (inter_x,inter_y+d)
-        min2 = (inter_x,inter_y-d)
+        if inter_y+d > max(line2[0][1], line2[1][1]):
+            max2 = (inter_x, max(line2[0][1], line2[1][1]))
+        else:
+            max2 = (inter_x,inter_y+d)
+        if inter_y-d < min(line2[0][1], line2[1][1]):
+            min2 = (inter_x, min(line2[0][1], line2[1][1]))
+        else:
+            min2 = (inter_x,inter_y-d)
     else:
         x2 = d/math.sqrt(m2*m2+1)
         y2 = m2*x2
-        max2 = (inter_x+x2,inter_y+y2)
-        min2 = (inter_x-x2,inter_y-y2)
+        if inter_x+x2 > max(line2[0][0], line2[1][0]):
+            if line2[0][0] > line2[1][0]:
+                max2 = (line2[0][0], line2[0][1])
+            else:
+                max2 = (line2[1][0], line2[1][1])
+        else:
+            max2 = (inter_x+x2,inter_y+y2)
+        if inter_x-x2 < min(line2[0][0], line2[1][0]):
+            if line2[0][0] < line2[1][0]:
+                min2 = (line2[0][0], line2[0][1])
+            else:
+                min2 = (line2[1][0], line2[1][1])
+        else:
+            min2 = (inter_x-x2,inter_y-y2)
+
 
     return min1, min2, max1, max2
 
