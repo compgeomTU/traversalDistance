e3b4d11e749096dc3dd11a2d1180b85aba138bb5
Rena Repenning <renareps108@gmail.com>
2022-04-19 19:06:37 -0500

    easy cases

--- compute_union.py
+++ compute_union.py
@@ -4,9 +4,18 @@
 #[(mycb.start_p, mycb.end_p)]
 
 def compute_union(intervals, mycb):
+    print("--my cbs: ", mycb, "  --intervals: ", intervals)
     Sx, Ex = mycb
-    print(mycb)
-    print(intervals)
+
+    # entirely before
+    if Sx > intervals[len(intervals)-1][1]:
+        intervals += [(mycb)]
+        return intervals
+    # entirely after
+    elif Ex < intervals[0][0]:
+        intervals = [(mycb)] + intervals
+        return intervals
+
     for i in range(len(intervals)):
         """check space before and in next interval"""
         if Sx <= intervals[i][0]:
@@ -18,10 +27,13 @@ def compute_union(intervals, mycb):
             # Sx becomes absorbed into this interval and then we look at the end
             Sx = intervals[i][0]
             break
-        else:
-            print("on to next interval")
 
-    for x in range(i, len(intervals), -1):
+    print("Sx = ", Sx)
+
+    print(i, len(intervals))
+    for x in range(0, len(intervals)):
+        print("x = ", x)
+        print("- ", intervals[x])
         if Ex >= intervals[x][1]:
             intervals[x] = (intervals[x][0], Ex)
             break
@@ -30,14 +42,17 @@ def compute_union(intervals, mycb):
             Ex = intervals[i][0]
             break
 
-    print(intervals)
-    # return intervals
+    # print(intervals)
+    return intervals
 
 
 # assume current intervals are sorted
-case1 = [(0, .1), (.2, .5), (.8, 1)], (.6, .7)
-case2 = [(0, .1), (.2, .5), (.7, .9)], (.6, .8)
-case3 = [(0, .1), (.2, .7),  (.8, .9)], (.6, .9)
+case1 = [(0, .1), (.2, .5), (.8, 1)], (.6, .7)  # works
+case2 = [(0, .1), (.2, .5), (.7, .9)], (.6, .8)  # works
+case3 = [(0, .1), (.2, .5)], (.6, .8)  # works
+case4 = [(.3, .4), (.6, .8)], (0, .1)  # works
 
-c = case3
-compute_union(c[0], c[1])
+case5 = [(.2, .7),  (.8, .9)], (.6, .85)
+
+c = case5
+print(compute_union(c[0], c[1]))
