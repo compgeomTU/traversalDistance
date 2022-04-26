e408bfdf50505133ca162e7b08fe7d62d46623a7
Rena Repenning <renareps108@gmail.com>
2022-04-26 17:06:37 -0500

    working on intervals

--- compute_union.py
+++ compute_union.py
@@ -20,18 +20,24 @@ def compute_union(intervals, mycb):
         intervals = [(mycb)] + intervals
         return intervals
 
+    inside_intervals = []
+
     for i in range(len(intervals)):
         """check space before and in next interval"""
         if Sx <= intervals[i][0]:
+            beginningIndex = i
             # change the current interval to start at Sx
             intervals[i] = (Sx, intervals[i][1])
             break  # and now find what we do with the larger
         elif Sx <= intervals[i][1]:
             # sx is in the middle of an interval
             # Sx becomes absorbed into this interval and then we look at the end
+            beginningIndex = i
             flag = "inside"
             Sx = intervals[i][0]
             break
+
+    print("beginning index: ", i)
 
     print("Sx = ", Sx)
 
@@ -39,10 +45,11 @@ def compute_union(intervals, mycb):
     # l = len(intervals)-1
     """for x in range(0, len(intervals), -1):  # THIS DOESNT WORK"""
 
-    for x in [1, 0]:
+    for x in [4, 3, 2, 1, 0]:
         print("\nEx=", Ex, end=" ")
         print("intervals[x]=", intervals[x], end=" ")
         if Ex >= intervals[x][1]:
+            endIndex = x-1
             intervals[x] = (intervals[x][0], Ex)
             """need to drop one to the right if"""
             # if x+1 < len(intervals):
@@ -50,6 +57,7 @@ def compute_union(intervals, mycb):
             #     intervals = intervals[:-1]
             # # break
         elif Ex >= intervals[x][0]:
+            endIndex = x-1
             if flag == "inside":
                 intervals[x] = (Sx, intervals[x][1])
                 # intervals.pop(x-1)
@@ -58,7 +66,7 @@ def compute_union(intervals, mycb):
                 # absorbed into interval
                 Ex = intervals[x][0]
             break
-
+    print("\nbeginning index=", beginningIndex, " end index=", endIndex)
     # print(intervals)
     return intervals
 
@@ -79,6 +87,8 @@ case9 = [(.01, .1), (.2, .5), (.7, .9)
 case8 = [(.01, .1), (.2, .5), (.7, .9)
          ], (.03, .8)  # spans multiple --> needs to drop one smaller
 
+case10 = [(0, .1), (.2, .3), (.4, .5), (.6, .7), (.8, .9)], (.25, .65)
 
-c = case9
+
+c = case10
 print("\nfinal = ", compute_union(c[0], c[1]))
