c70a24ca6f2c7cb8c1b8b9f9176d8fb6e407bf9f
Will Rodman <willc.rodman@gmail.com>
2023-04-24 22:54:05 -0500

    updated read me and main

--- main.py
+++ main.py
@@ -8,41 +8,42 @@ import sys
 from Graph import Graph
 from FreeSpaceGraph import FreeSpaceGraph
 
-filename1 = sys.argv[1]
-filename2 = sys.argv[2]
-epsilon = float(sys.argv[3])
+if __name__ == "__main__":
+    filename1 = sys.argv[1]
+    filename2 = sys.argv[2]
+    epsilon = float(sys.argv[3])
 
-if '-l' in sys.argv:
-    log = True
-else:
-    log = False
+    if '-l' in sys.argv:
+        log = True
+    else:
+        log = False
 
-if '-p' in sys.argv:
-    plot = True
-else:
-    plot = False
+    if '-p' in sys.argv:
+        plot = True
+    else:
+        plot = False
 
-g1 = Graph(filename1)
-g2 = Graph(filename2)
+    g1 = Graph(filename1)
+    g2 = Graph(filename2)
 
-fsg = FreeSpaceGraph(g1, g2, epsilon, filename1=filename1, filename2=filename2, log=log)
+    fsg = FreeSpaceGraph(g1, g2, epsilon, filename1=filename1, filename2=filename2, log=log)
 
-if plot:
-    fsg.plot()
+    if plot:
+        fsg.plot()
 
-projection_check = fsg.DFSTraversalDist()
+    projection_check = fsg.DFSTraversalDist()
 
-print("\n-- Epsilon --")
-print(f"     {epsilon}")
+    print("\n-- Epsilon --")
+    print(f"     {epsilon}")
 
-print("\n-- Sample Graphs --")
-print(f"     No. edges in {filename1}:", g1.numberOfEdges)
-print(f"     No. vertices in {filename1}:", g1.numberOfNodes)
-print(f"     No. edges in {filename2}:", g2.numberOfEdges)
-print(f"     No. vertices in {filename2}:", g1.numberOfNodes)
+    print("\n-- Sample Graphs --")
+    print(f"     No. edges in {filename1}:", g1.numberOfEdges)
+    print(f"     No. vertices in {filename1}:", g1.numberOfNodes)
+    print(f"     No. edges in {filename2}:", g2.numberOfEdges)
+    print(f"     No. vertices in {filename2}:", g1.numberOfNodes)
 
-print("\n -- Running Traversal Distance --")
-print("     DFS Function Calls:", fsg.DFS_calls)
-print("     DFS FS CBs computed:", len(fsg.cell_boundaries))
-print("     Total Possible CBs:", fsg.cb_count)
-print("     DFS Projection Check:", projection_check)
+    print("\n -- Running Traversal Distance --")
+    print("     DFS Function Calls:", fsg.DFS_calls)
+    print("     DFS FS CBs computed:", len(fsg.cell_boundaries))
+    print("     Total Possible CBs:", fsg.cb_count)
+    print("     DFS Projection Check:", projection_check)
